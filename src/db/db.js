d0449d889efd5f188eca991ab2bd2797a47fffdc
Bittu Kumar <bk7355583@gmail.com>
2024-05-06 17:10:51 +0530

    recommited

--- src/db/db.js
+++ src/db/db.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose')
-const URL=process.env.MONGO_URL || "mongodb://localhost:27017/aliexpressDb"
+const URL=process.env.MONGO_URL 
 mongoose
   .connect(URL)
   .then(() => {
